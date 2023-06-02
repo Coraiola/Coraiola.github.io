0a0cb984446be1aa6a8915ebc399e887233d8f57
Coraiola <85491364+Coraiola@users.noreply.github.com>
2023-06-02 14:49:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Rolagem suave ao clicar nos links de navegação
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
